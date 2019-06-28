d95a9dd73cdb9e0cdead23dfe4f2d73dee63e883
sy <sunyu_98720@163.com>
2019-06-28 09:44:02 +0800

    testDemo

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -1,14 +1,27 @@
 package life.majiang.community.community.controller;
 
+import life.majiang.community.community.dto.AccessTokenDTO;
+import life.majiang.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id("3d2698f5434982a26b41");
+        accessTokenDTO.setClient_secret("600e93dd63d9a1d5af9dc88e4665bf7081c6543a");
+        accessTokenDTO.setRedirect_uri("http://localhost:8081/callback");
+        accessTokenDTO.setState(state);
+        githubProvider.getAccessToken(accessTokenDTO);
         return "index";
     }
 }
