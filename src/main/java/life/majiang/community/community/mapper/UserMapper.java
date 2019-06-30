e9972dc1df67067d84eb933c06a4f78dab28e4b6
sy <sunyu_98720@163.com>
2019-06-30 23:58:28 +0800

    mysq配置及数据插入

--- src/main/java/life/majiang/community/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/community/mapper/UserMapper.java
@@ -5,6 +5,6 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user(name,account_id,gmt_create_gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 }
