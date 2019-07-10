b8a85903dbcbd07a977d1d4dcf42fe8d20741af3
sy <sunyu_98720@163.com>
2019-07-11 00:27:24 +0800

    lombox插件使用,url插入

--- src/main/java/life/majiang/community/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/community/controller/PublishController.java
@@ -31,30 +31,31 @@ public class PublishController {
     @PostMapping("publish")
     public String doPublish(
             @RequestParam(value = "title",required = false) String title,
-            @RequestParam("description") String description,
-            @RequestParam("tag") String tag,
+            @RequestParam(value = "description",required = false) String description,
+            @RequestParam(value = "tag",required = false) String tag,
             HttpServletRequest request,
             Model model){
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
 
-        if(title == null || title == " "){
+        if(title == null || title == ""){
             model.addAttribute("error","标题不能为空");
             return "publish";
         }
-        if(description == null || description == " "){
+        if(description == null || description == ""){
             model.addAttribute("error","问题补充不能为空");
             return "publish";
         }
-        if(tag == null || tag == " "){
+        if(tag == null || tag == ""){
             model.addAttribute("error","标签不能为空");
             return "publish";
         }
 
-        model.addAttribute("title",title);
-        model.addAttribute("description",description);
-        model.addAttribute("tag",tag);
+
         User user = null;
         Cookie[] cookies = request.getCookies();
-        if(cookies != null){
+        if(cookies != null && cookies.length != 0){
             for(Cookie cookie : cookies){
                 if (cookie.getName().equals("token")){
                     String token = cookie.getValue();
@@ -71,13 +72,13 @@ public class PublishController {
             return "publish";
         }
         Question question = new Question();
-        questionMapper.create(question);
         question.setTitle(title);
         question.setDescription(description);
         question.setTag(tag);
         question.setCreator(user.getId());
         question.setGmtCreate(System.currentTimeMillis());
         question.setGmtModified(question.getGmtCreate());
+        questionMapper.create(question);
         return "redirect:/";
     }
 
